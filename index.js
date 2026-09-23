82540e0b471e4cc048cca45266f2e6f7609e00d9
Lucas-Sousa <lucas.sousa555@aluno.ce.gov.br>
2026-09-23 13:38:13 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+const estudou = "não"
+const trabalhou = "sim"
+const treinou = "não"
+const arrumouQuarto = "não"
+const descansou = "sim"
+
+if (estudou == "sim") {
+    console.log("Você estudou hoje")
+} else {
+    console.log("Você precisa estudar")
+}
+
+if (trabalhou == "sim") {
+    console.log("Você trabalhou hoje")
+} else {
+    console.log("Você precisa trabalhar")
+}
+
+if (treinou == "sim") {
+    console.log("Você treinou hoje")
+} else {
+    console.log("Você precisa treinar")
+}
+
+if (arrumouQuarto == "sim") {
+    console.log("Você arrumou o quarto")
+} else {
+    console.log("Você precisa arrumar o quarto")
+}
+
+if (descansou == "sim") {
+    console.log("Você descansou hoje")
+} else {
+    console.log("Você precisa descansar")
+}
